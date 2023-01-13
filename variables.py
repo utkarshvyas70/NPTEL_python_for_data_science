ba7f2211fdb7b2541065f1ba0644a1dcd6f8babd
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-01-13 21:36:04 +0530

    py file

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Camelcaseing
+ageEmp=45
+AgeEmp=45
+
+# Snake
+age_emp=45
+Age_emp=45
+
+# Pascal
+AgeEmp=45
+
+# can assign different variables in single line
+Physics,Chemistry,Maths=89,90,75
+
