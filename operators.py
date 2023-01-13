2557646fb65fc530a5a822cf22b9a688dbd1d2de
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-01-13 21:36:47 +0530

    py file

---
+++ operators.py
@@ -0,0 +1,43 @@
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+# Hierarcy of operators
+# PEDMAS
+
+a+=b
+# a=a+b
+print(a)
+a-=b
+# a=a-b
+print(a)
+a*=b
+# a=a*b
+print(a)
+a/=b
+# a=a/b
+print(a)
+
+
+x=5
+y=7
+print(x>y)
+print(x<y)
+print(x<=y)
+print(x>=y)
+print(x==y)
+print(x!=y)
+
+
+print((x>y) or (x<y))
+print((x>y) or (x<y))
+print((x>y) and (x<y))
+print(not(x==y))
+
+
+print((x<y) | (x==y))
+print((x<y) & (x==y))
